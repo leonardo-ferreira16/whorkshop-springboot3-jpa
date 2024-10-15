fc763f6289f559d3968da4a432dbb5fb8fc42b8e
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-10-15 15:53:40 -0300

    User insert

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entities.User;
 import com.example.demo.services.UserService;
@@ -19,7 +23,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@GetMapping
+	@GetMapping //método http para recuperar os dados do banco de dados
 	public ResponseEntity<List<User>> findAll(){
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);		
@@ -29,5 +33,12 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping //método http para inserir
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 }
